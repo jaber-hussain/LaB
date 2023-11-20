5ff102d1701924869e00d3fde78d3869b2e3d1b3
jaber-hussain <130204260+jaber-hussain@users.noreply.github.com>
2023-11-20 09:23:57 +0500

    Add files via upload

---
+++ BasicProgram.java
@@ -0,0 +1,13 @@
+public class BasicProgram{
+	public static void main(String[] args){
+		int age=25;
+		double height=5.9;
+		String name="John Doe";
+		boolean isAdult=true;
+		
+		System.out.println("Name:" +name);
+		System.out.println("Age:" +age);
+		System.out.println("Height:" +height);
+		System.out.println("Adult:" +isAdult);
+	}
+}
